4510c3ff52cb34075553b60dd4aab4ee5c90f8f0
Alex Silva <a0690349@gmail.com>
2025-03-03 09:30:16 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cinema_pedia/config/router/app_router.dart';
+
+import 'package:flutter/material.dart';
+import 'config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
+
+
